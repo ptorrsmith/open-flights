4ff825fb44fccdc5dbbf59abd4048a928c601aa6
Peter Torr Smith <peter.torr.smith@cultureamp.com>
2021-10-12 20:17:12 +1300

    rails g model Airline name image_url slug rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_10_12_071401) do
+
+  create_table "airlines", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
